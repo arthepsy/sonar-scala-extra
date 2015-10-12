8cb4e71656d9fa5b4fb5055b4f370880d4d9c497
Andris Raugulis <moo@arthepsy.eu>
2015-10-12 11:48:38 +0300

    Fix resource class.

--- sonar-scapegoat-plugin/src/main/java/eu/arthepsy/sonar/plugins/scapegoat/rule/ScapegoatQualityProfile.java
+++ sonar-scapegoat-plugin/src/main/java/eu/arthepsy/sonar/plugins/scapegoat/rule/ScapegoatQualityProfile.java
@@ -45,7 +45,7 @@ public class ScapegoatQualityProfile extends ProfileDefinition {
     private static final String SCAPEGOAT_PROFILE = "Sonar way";
 
     protected InputStream getRulesStream() {
-        return getClass().getResourceAsStream(ScapegoatConfiguration.RULES_FILE);
+        return ScapegoatQualityProfile.class.getClass().getResourceAsStream(ScapegoatConfiguration.RULES_FILE);
     }
 
     @Override
